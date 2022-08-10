7c16bfc8bffb9bb106f88427898ac7feaf374ee0
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-08-10 12:13:23 -0300

    feat: :fire: add req13 - create index.ts (now it's passing!)

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,15 @@ const player1 = new Character('Sushi');
 const player2 = new Character('Spoleta');
 const player3 = new Character('Boris');
 
-player1.levelUp();
-player1.levelUp();
-
 const monster1 = new Monster();
 const monster2 = new Dragon();
 
-const pvp = new PVP(player1, player3);
+const pvp = new PVP(player2, player3);
 const pve = new PVE(player1, [monster1, monster2]);
+
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
 
 const runBattles = (battles: Battle[]) => {
   battles.forEach((battle) => {
@@ -22,13 +23,4 @@ const runBattles = (battles: Battle[]) => {
   });
 };
 
-export {
-  player1,
-  player2,
-  player3,
-  monster1,
-  monster2,
-  pvp,
-  pve,
-  runBattles,
-};
+export { player1, player2, player3, monster1, monster2, pvp, pve, runBattles };
